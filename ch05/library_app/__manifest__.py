b0434ca9e5df7ce35762a7576ccd06aaf65dc440
Daniel Reis <dreis.pt@hotmail.com>
2018-12-26 18:18:34 +0000

    Ch05 - Data files (final code)

--- ch05/library_app/__manifest__.py
+++ ch05/library_app/__manifest__.py
@@ -9,6 +9,11 @@
     'views/book_view.xml',
     'views/book_list_template.xml',
  ],
+ 'demo': [
+    'data/res.partner.csv',
+    'data/library.book.csv',
+    'data/book_demo.xml',
+ ],
  'application': True,
  'installable': True,
- }
+}
