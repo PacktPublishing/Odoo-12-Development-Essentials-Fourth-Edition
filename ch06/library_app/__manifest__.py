062d74112713f101ea0b8effd1a8b4d05506b627
Daniel Reis <dreis.pt@hotmail.com>
2018-12-26 18:23:18 +0000

    Ch06 - Models (initial code)

---
+++ ch06/library_app/__manifest__.py
@@ -0,0 +1,19 @@
+{'name': 'Library Management Application',
+ 'description': 'Library books, members and book borrowing.',
+ 'author': 'Daniel Reis',
+ 'depends': ['base'],
+ 'data': [
+    'security/library_security.xml',
+    'security/ir.model.access.csv',
+    'views/library_menu.xml',
+    'views/book_view.xml',
+    'views/book_list_template.xml',
+ ],
+ 'demo': [
+    'data/res.partner.csv',
+    'data/library.book.csv',
+    'data/book_demo.xml',
+ ],
+ 'application': True,
+ 'installable': True,
+}
