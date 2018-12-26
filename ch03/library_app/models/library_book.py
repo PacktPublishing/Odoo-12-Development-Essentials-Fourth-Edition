762414a32188a74fb9e0140ae9ed5fab59205cb4
Daniel Reis <dreis.pt@hotmail.com>
2018-12-26 18:02:10 +0000

    Ch03 - First app

---
+++ ch03/library_app/models/library_book.py
@@ -0,0 +1,38 @@
+from odoo import api, fields, models
+from odoo.exceptions import Warning
+
+
+class Book(models.Model):
+    _name = 'library.book'
+    _description = 'Book'
+
+    name = fields.Char('Title', required=True)
+    isbn = fields.Char('ISBN')
+    active = fields.Boolean('Active?', default=True)
+    date_published = fields.Date()
+    image = fields.Binary('Cover')
+    publisher_id = fields.Many2one('res.partner', string='Publisher')
+    author_ids = fields.Many2many('res.partner', string='Authors')
+
+    @api.multi
+    def _check_isbn(self):
+        """Check one Book's ISBN"""
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderators = [1, 3] * 6
+            total = sum(a * b for a, b in zip(digits[:12], ponderators))
+            remain = total % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
+    @api.multi
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise Warning(
+                    'Please provide an ISBN13 for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning(
+                    '%s is an invalid ISBN' % book.isbn)
+        return True
